2fd411aeadfa2bb58a8b774011fbd1a630fa29ec
edeepaku <user@DESKTOP-IUK9U5L.>
2025-01-14 20:39:35 +0530

    adding file

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # demoApp
+
+wsl -d podman-machine-default
+cd /mnt/f/Python_Proj
+podman build -t tag .
+podman run -it <image-name> -d
---
+++ demo.py
@@ -0,0 +1,8 @@
+import time
+import sys
+a=0
+while(True):
+    print(f"print variable value as {a}")
+    sys.stdout.flush()
+    a=a+1
+    time.sleep(4)
